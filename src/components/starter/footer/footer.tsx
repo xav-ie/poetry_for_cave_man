d369e83bf8740875541cc86edb35857cefe68281
Xavier Ruiz <xruiz@bu.edu>
2023-12-31 23:05:36 -0600

    feat(layout): basic layout set-up

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,18 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "~/routes/layout";
-import styles from "./footer.module.css";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
-
   return (
     <footer>
       <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
-        </a>
+        Made with ❤️ by <Link href="https://xav.ie">Xav.ie</Link>r
       </div>
     </footer>
   );
